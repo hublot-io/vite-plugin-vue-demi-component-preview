e53c9dfa968fca5a59c2c41f09608f18938c7ee8
Alexandre BERTRAND <alexandre@hublot.io>
2024-07-26 11:55:36 +0200

    add compatibilty with vue 2.7

--- client/index.ts
+++ client/index.ts
@@ -1,26 +1,53 @@
-import { App, defineAsyncComponent, DefineComponent, h } from 'vue-demi';
+import * as vue from "vue-demi";
+import { DefineComponent } from 'vue-demi';
 
-export default function (app: App) {
+const { h } = vue;
+const { isVue2 } = vue
+const defineAsyncComponent = vue.defineAsyncComponent;
+
+//@ts-ignore
+export default function (app) {
 
 	const location = globalThis.location;
 
 	if (location && location.pathname.includes('/__preview/')) {
 
 		const importPath = location.pathname.replace('/__preview', '');
-		const Component = defineAsyncComponent(() => import(/* @vite-ignore */importPath));
-		const Layout = defineAsyncComponent(() => import(/* @vite-ignore */importPath + '__preview.vue'));
-
+		
+		
 		if (import.meta.hot) {
 			fireHash();
 			window.addEventListener('hashchange', fireHash);
 		}
+	
 
-		(app._component as DefineComponent).setup = () => {
-			return () => h(Layout, undefined, {
-				default: (props: any) => h(Component, props)
-			});
-		};
+		if(isVue2){
+			import(/* @vite-ignore */importPath).then(module => {
+				import(/* @vite-ignore */importPath + '__preview.vue').then(previewModule => {
+					const componentName = importPath.split("/")
 
+					app.component = {
+						[componentName[componentName.length-1].split(".")[0]] : module.default,
+						'PreviewComponent': previewModule.default
+					} 
+					//@ts-ignore
+					app.compnent = previewModule.default
+				})
+			})
+		} else {
+			const Component = defineAsyncComponent(() => import(/* @vite-ignore */importPath));
+			const Layout = defineAsyncComponent(() => import(/* @vite-ignore */importPath + '__preview.vue'));
+			
+			(app._component as DefineComponent).setup = () => {
+				return () => h(Layout, undefined, {
+				//@ts-ignore
+					default: (props: any) => h(Component, props)
+				});
+			};
+	
+		}
+		
+		
 		function fireHash() {
 			try {
 				import.meta.hot?.send('vue-component-preview:hash', {
